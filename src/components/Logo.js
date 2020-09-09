fd17d8b29f9cafa04dcd18ca947733a8c043fbf9
Aratramba <aratramba@gmail.com>
2020-09-09 13:57:19 +0200

    fix font loading

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -18,6 +18,57 @@ const styles = `
 
 const Logo = (props) => (
   <span className="logoWrapper">
+    <style>{`
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap");
+  
+    @font-face {
+      font-display: block;
+      font-family: "GT America";
+      font-style: normal;
+      font-weight: 400;
+      src: url("https://eight.nl/fonts/GT-America-Standard-Regular.woff2")
+          format("woff2"),
+        url("https://eight.nl/fonts/GT-America-Standard-Regular.woff")
+          format("woff"),
+        url("https://eight.nl/fonts/GT-America-Standard-Regular.ttf")
+          format("truetype");
+    }
+    
+    @font-face {
+      font-display: block;
+      font-family: "GT America";
+      font-style: normal;
+      font-weight: 500;
+      src: url("https://eight.nl/fonts/GT-America-Standard-Medium.woff2")
+          format("woff2"),
+        url("https://eight.nl/fonts/GT-America-Standard-Medium.woff") format("woff"),
+        url("https://eight.nl/fonts/GT-America-Standard-Medium.ttf")
+          format("truetype");
+    }
+    
+    @font-face {
+      font-display: block;
+      font-family: "GT America";
+      font-style: normal;
+      font-weight: 700;
+      src: url("https://eight.nl/fonts/GT-America-Standard-Bold.woff2")
+          format("woff2"),
+        url("https://eight.nl/fonts/GT-America-Standard-Bold.woff") format("woff"),
+        url("https://eight.nl/fonts/GT-America-Standard-Bold.ttf")
+          format("truetype");
+    }
+    
+    @font-face {
+      font-display: block;
+      font-family: "GT Super";
+      font-style: normal;
+      font-weight: 700;
+      src: url("https://eight.nl/fonts/GT-Super-Display-Bold.woff2") format("woff2"),
+        url("https://eight.nl/fonts/GT-Super-Display-Bold.woff") format("woff"),
+        url("https://eight.nl/fonts/GT-Super-Display-Bold.ttf") format("truetype");
+    }
+    `}
+    </style>
     <style type="text/css">{styles}</style>
     <svg width={122} height={80} viewBox="0 0 122 80" className="logoImage">
       <path fill="none" d="M0 0h122v80H0z" />
