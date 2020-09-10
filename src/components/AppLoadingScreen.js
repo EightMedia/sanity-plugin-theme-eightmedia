ad5e4d2f2ef13a09bfa37972b6ce41fd47844cab
Aratramba <aratramba@gmail.com>
2020-09-10 16:43:31 +0200

    make loader css instead of svg
    it's didn't play until the page was fully loaded because svg timelineBegin isn't supported, which defeats its purpose

--- src/components/AppLoadingScreen.js
+++ src/components/AppLoadingScreen.js
@@ -29,6 +29,25 @@ const AppLoadingScreenStyles = `
     margin-top: 12;
     font-weight: normal;
   }
+
+  .pulse {
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    background-color: transparent;
+    border: 3px solid white;
+    border-radius: 100%;
+    animation: pulse 1.2s infinite cubic-bezier(0.455, 0.03, 0.515, 0.955); 
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(0); 
+    } 100% {
+      transform: scale(1);
+      opacity: 0; 
+    }
+  }
 `;
 
 export default class AppLoadingScreen extends React.PureComponent {
@@ -45,59 +64,7 @@ export default class AppLoadingScreen extends React.PureComponent {
       <div className="loader-wrapper">
         <div className="loader-inner">
           <style dangerouslySetInnerHTML={{ __html: AppLoadingScreenStyles }}/>
-          <svg
-            width={44}
-            height={44}
-            viewBox="0 0 44 44"
-            stroke="#fff"
-          >
-            <g fill="none" fillRule="evenodd" strokeWidth={2}>
-              <circle cx={22} cy={22} r={1}>
-                <animate
-                  attributeName="r"
-                  begin="0s"
-                  dur="1.8s"
-                  values="1; 20"
-                  calcMode="spline"
-                  keyTimes="0; 1"
-                  keySplines="0.165, 0.84, 0.44, 1"
-                  repeatCount="indefinite"
-                />
-                <animate
-                  attributeName="stroke-opacity"
-                  begin="0s"
-                  dur="1.8s"
-                  values="1; 0"
-                  calcMode="spline"
-                  keyTimes="0; 1"
-                  keySplines="0.3, 0.61, 0.355, 1"
-                  repeatCount="indefinite"
-                />
-              </circle>
-              <circle cx={22} cy={22} r={1}>
-                <animate
-                  attributeName="r"
-                  begin="-0.9s"
-                  dur="1.8s"
-                  values="1; 20"
-                  calcMode="spline"
-                  keyTimes="0; 1"
-                  keySplines="0.165, 0.84, 0.44, 1"
-                  repeatCount="indefinite"
-                />
-                <animate
-                  attributeName="stroke-opacity"
-                  begin="-0.9s"
-                  dur="1.8s"
-                  values="1; 0"
-                  calcMode="spline"
-                  keyTimes="0; 1"
-                  keySplines="0.3, 0.61, 0.355, 1"
-                  repeatCount="indefinite"
-                />
-              </circle>
-            </g>
-          </svg>
+          <div className="pulse" />
           <div className="loader-text">{this.props.text}</div>
         </div>
       </div>
