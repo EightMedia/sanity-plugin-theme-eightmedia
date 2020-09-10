4c3169eb127b30863f0ce6f27b0f6dc1bd6bba99
Aratramba <aratramba@gmail.com>
2020-09-10 09:00:47 +0200

    fix app loading screen

--- src/components/AppLoadingScreen.js
+++ src/components/AppLoadingScreen.js
@@ -1,32 +1,34 @@
 /* eslint-disable max-len */
 import PropTypes from "prop-types";
 import React from "react";
-import { Loader } from "./Loader";
 
-const styles = `
-.wrapper {
-  height: "100vh";
-  width: "100vw";
-  position: "absolute";
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: "flex";
-  background: "#bee2ea";
-  color: "#222";
-},
+const AppLoadingScreenStyles = `
+ .loader-wrapper {
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    background: #bee2ea;
+    color: #222;
+    font-family: GT America, Roboto, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-.inner {
-  margin: "auto";
-  textAlign: "center";
-}
-
-.text {
-  display: "block";
-  marginTop: 12;
-  fontWeight: "normal";
-},
+  .loader-inner {
+    margin: auto;
+    text-align: center;
+  }
+  
+  .loader-text: {
+    display: block;
+    margin-top: 12;
+    font-weight: normal;
+  }
 `;
 
 export default class AppLoadingScreen extends React.PureComponent {
@@ -40,11 +42,63 @@ export default class AppLoadingScreen extends React.PureComponent {
 
   render() {
     return (
-      <div className="wrapper">
-        <style type="text/css">{styles}</style>
-        <div className="inner">
-          <Loader />
-          <div className="text">{this.props.text}</div>
+      <div className="loader-wrapper">
+        <div className="loader-inner">
+          <style dangerouslySetInnerHTML={{ __html: AppLoadingScreenStyles }}/>
+          <svg
+            width={44}
+            height={44}
+            viewBox="0 0 44 44"
+            stroke="#fff"
+          >
+            <g fill="none" fillRule="evenodd" strokeWidth={2}>
+              <circle cx={22} cy={22} r={1}>
+                <animate
+                  attributeName="r"
+                  begin="0s"
+                  dur="1.8s"
+                  values="1; 20"
+                  calcMode="spline"
+                  keyTimes="0; 1"
+                  keySplines="0.165, 0.84, 0.44, 1"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="stroke-opacity"
+                  begin="0s"
+                  dur="1.8s"
+                  values="1; 0"
+                  calcMode="spline"
+                  keyTimes="0; 1"
+                  keySplines="0.3, 0.61, 0.355, 1"
+                  repeatCount="indefinite"
+                />
+              </circle>
+              <circle cx={22} cy={22} r={1}>
+                <animate
+                  attributeName="r"
+                  begin="-0.9s"
+                  dur="1.8s"
+                  values="1; 20"
+                  calcMode="spline"
+                  keyTimes="0; 1"
+                  keySplines="0.165, 0.84, 0.44, 1"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="stroke-opacity"
+                  begin="-0.9s"
+                  dur="1.8s"
+                  values="1; 0"
+                  calcMode="spline"
+                  keyTimes="0; 1"
+                  keySplines="0.3, 0.61, 0.355, 1"
+                  repeatCount="indefinite"
+                />
+              </circle>
+            </g>
+          </svg>
+          <div className="loader-text">{this.props.text}</div>
         </div>
       </div>
     );
--- src/components/FontStyles.js
+++ src/components/FontStyles.js
@@ -1,25 +1,5 @@
-import React from "react";
-const config = "config:sanity";
-
-const styles = `
-  .logoWrapper {
-    display: flex;
-    align-items: center;
-  }
-  
-  .logoImage {
-    margin-right: 8px;
-  }
-  
-  .logoName {
-    font-weight: 500;
-  }
-`
-
-const Logo = (props) => (
-  <span className="logoWrapper">
-    <style type="text/css">{styles}</style>
-    <style>{`
+export default FontStyles = () => (
+  <style>{`
     @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap");
 
     @font-face {
@@ -69,18 +49,4 @@ const Logo = (props) => (
         url("https://eight.nl/fonts/GT-Super-Display-Bold.ttf") format("truetype");
     }
     `}</style>
-    <svg width={122} height={80} viewBox="0 0 122 80" className="logoImage">
-      <path fill="none" d="M0 0h122v80H0z" />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M46.98 79.998l21.547-.006L85.44 0H63.893L46.98 79.998zM83.448 80h21.547L121.905.01 100.359 0 83.448 80zM41.929 33.339l-18.237-.006 3.881-18.331 18.228.002-3.872 18.335zM17 64.997h18.23l3.882-18.33H20.873L17 64.996z"
-        fill="white"
-      />
-    </svg>
-
-    {/* <span className="logoName">{config.project.name}</span> */}
-  </span>
 );
-
-export default Logo;
